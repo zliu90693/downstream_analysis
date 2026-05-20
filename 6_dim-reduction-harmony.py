4a6d4d28d9487f9b89f3cf36eccbafe9c8284095
zliu <zliu90693@gmail.com>
2026-05-20 20:00:15 +0800

    try modifing

--- 6_dim-reduction-harmony.py
+++ 6_dim-reduction-harmony.py
@@ -2,6 +2,7 @@
 
 import scanpy as sc
 import anndata
+import harmonypy as hm
 
 # %%
 
@@ -13,12 +14,6 @@ def run_pca(
     adata.var["highly_variable"] = adata.var["highly_deviant"]
     sc.pp.pca(adata, svd_solver="arpack", mask_var="highly_variable")
 
-def visualize_pca(
-    adata: anndata.AnnData,
-    color
-) -> None:
-    sc.pl.pca_scatter(adata, color=color)
-
 # %%
 
 def run_tsne(
@@ -27,11 +22,6 @@ def run_tsne(
 ) -> None:
     sc.tl.tsne(adata, use_rep=use_rep)
 
-def visualize_tsne(
-    adata: anndata.AnnData,
-    color
-) -> None:
-    sc.pl.tsne(adata, color=color)
 
 # %%
 
@@ -41,11 +31,6 @@ def run_umap(
     sc.pp.neighbors(adata)
     sc.tl.umap(adata)
 
-def visualize_umap(
-    adata: anndata.AnnData,
-    color,
-) -> None:
-    sc.pl.umap(adata, color=color)
 
 # %%
 def run_harmony(
@@ -54,8 +39,29 @@ def run_harmony(
     use_rep: str = "X_pca",
     adjusted_basis: str = 'X_pca_harmony'
 ) -> None:
-    sc.external.pp.harmony_integrate(adata, key=batch_key, basis=use_rep, adjusted_basis=adjusted_basis)
+    # sc.external.pp.harmony_integrate(adata, key=batch_key, basis=use_rep, adjusted_basis=adjusted_basis)
 
+    pcs = adata.obsm[use_rep] # (n_cells, n_pcs)
+    # Run Harmony
+    harmony_out = hm.run_harmony(pcs, adata.obs, batch_key)
+    # Z_corr is already a numpy array with shape (n_cells, n_pcs)
+    adata.obsm[adjusted_basis] = harmony_out.Z_corr
+    # Use harmonized PCs for downstream analysis
+    # sc.pp.neighbors(adata, use_rep='X_pca_harmony')
+    # sc.tl.umap(adata)
+
+# %%
+
+def visualize(
+    adata: anndata.AnnData,
+    basis: str,
+    color,
+) -> None:
+    sc.pl.embedding(
+        adata,
+        basis=basis,
+        color=color,
+    )
 
 # %%
 
@@ -69,24 +75,20 @@ combined_h5ad = sc.read_h5ad(f"./{project_name}/5_feature-selection-output/conca
 run_pca(combined_h5ad, layer="scran_normalization")
 # %%
 
-visualize_pca(combined_h5ad, color="total_counts") # ????
-visualize_pca(combined_h5ad, color="batch")
+visualize(combined_h5ad, basis="X_pca", color=["total_counts", "pct_counts_mt", "batch"])
 sc.pl.pca_loadings(combined_h5ad, components=[1, 2])
-visualize_pca(combined_h5ad, color="pct_counts_mt")
 
 # %%
 
 run_tsne(combined_h5ad, use_rep="X_pca")
 
 # %%
-visualize_tsne(combined_h5ad, color="total_counts")
-visualize_tsne(combined_h5ad, color="batch")
-visualize_tsne(combined_h5ad, color="pct_counts_mt")
+visualize(combined_h5ad, basis="X_tsne", color=["total_counts", "pct_counts_mt", "batch"])
 
 # %%
 run_umap(combined_h5ad)
 # %%
-visualize_umap(combined_h5ad, color=["total_counts", "pct_counts_mt", "batch"])
+visualize(combined_h5ad, basis="X_umap", color=["total_counts", "pct_counts_mt", "batch"])
 
 # %%
 run_harmony(combined_h5ad, batch_key="batch", use_rep="X_pca", adjusted_basis='X_pca_harmony')
